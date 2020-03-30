02effaafcab0be6d8987de6fc1873dfc15e4722e
pulze <malagodap2@gmail.com>
2020-03-30 10:59:17 +0530

    new changes..

--- express/server.js
+++ express/server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-var router = express.Router();
 
 // ROUTES
 var indexRouter = require('./routes/index');
@@ -13,16 +12,12 @@ var portfoliosRouter = require('./routes/portfolios');
 var resumeRouter = require('./routes/resume');
 var sendRouter = require('./routes/send');
 
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use(bodyParser.json());  // path must route to lambda
 app.use(express.static(path.join(__dirname, '../public')));
 
 
 // ROUTES
-app.use('/', indexRouter);
+app.use('/.netlify/functions/', indexRouter);
 app.use('/.netlify/functions/sidenav', sidenavRouter);
 app.use('/.netlify/functions/portfolios', portfoliosRouter);
 app.use('/.netlify/functions/resume', resumeRouter);
