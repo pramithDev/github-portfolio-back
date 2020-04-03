86d6df77ac61640ffaf151a5bde5ba20fb7787d2
pulze <malagodap2@gmail.com>
2020-04-03 08:04:39 +0530

    new portfolios added..

--- db/dbPortfolios.js
+++ db/dbPortfolios.js
@@ -195,144 +195,79 @@ let portfolios =  [
     },
     {
         id: 3,
-        link_name:"project2",
-        name:"Task Tracker and Leave Management Web Application",
+        link_name:"project3",
+        name:"Media Firm Landing Page and Web",
         work_type:"UI/UX & Front-End Development",
-        thumb_image:"task-tracker/thumb.jpg",
+        thumb_image:"media/thumb.jpg",
         description:"Lorem ipsum lorem ipsum hello world",
-        technologies:['HTML5','CSS3','Responsive Design','JavaScript','BootStrap','Jquery','Express JS','Sass','Gulp'],
+        technologies:['HTML5','CSS3','Responsive Design','JavaScript','BootStrap','Jquery','Sass','Gulp'],
         ui_images:[
             {
                 id: 1,
-                name: "task-tracker/1login.png"
+                name: "media/Landing/Landing.png"
             },
             {
                 id: 2,
-                name: "task-tracker/2task-dashboard.png"
+                name: "media/Landing/Landing-2.png"
             },
             {
                 id: 3,
-                name: "task-tracker/3users_timelog.png"
+                name: "media/Web/home.png"
             },
             {
                 id: 4,
-                name: "task-tracker/4users_timelog2.png"
+                name: "media/Web/home-new.png"
+            }
+        ]
+    },
+    {
+        id: 4,
+        link_name:"project4",
+        name:"NGO web Site",
+        work_type:"UI/UX & Front-End Development",
+        thumb_image:"ngo/thumb.jpg",
+        description:"Lorem ipsum lorem ipsum hello world",
+        technologies:['HTML5','CSS3','Responsive Design','JavaScript','BootStrap','Jquery','Sass','Gulp'],
+        ui_images:[
+            {
+                id: 1,
+                name: "ngo/1Home-w.png"
+            },
+            {
+                id: 2,
+                name: "ngo/2About-Us.png"
+            },
+            {
+                id: 3,
+                name: "ngo/3contact.png"
+            },
+            {
+                id: 4,
+                name: "ngo/4donate.png"
             },
             {
                 id: 5,
-                name: "task-tracker/5users_leave.png"
+                name: "ngo/5gallary.png"
             },
             {
                 id: 6,
-                name: "task-tracker/6admin_addemployees.png"
+                name: "ngo/6philo.png"
             },
             {
                 id: 7,
-                name: "task-tracker/7admin_addemployees2.png"
+                name: "ngo/7public.png"
             },
             {
                 id: 8,
-                name: "task-tracker/8admin_addemployees3.png"
+                name: "ngo/8modal.png"
             },
             {
                 id: 9,
-                name: "task-tracker/9admin_addemployees4.png"
+                name: "ngo/9mobile-menu.png"
             },
             {
                 id: 10,
-                name: "task-tracker/10admin_calendar.png"
-            },
-            {
-                id: 11,
-                name: "task-tracker/11admin_calendar2.png"
-            },
-            {
-                id: 12,
-                name: "task-tracker/12admin_calendar3.png"
-            },
-            {
-                id: 13,
-                name: "task-tracker/13admin_chargecodes.png"
-            },
-            {
-                id: 14,
-                name: "task-tracker/14admin_chargecodes2.png"
-            },
-            {
-                id: 15,
-                name: "task-tracker/15admin_chargecodes3.png"
-            },
-            {
-                id: 16,
-                name: "task-tracker/16admin_chargecodes4.png"
-            },
-            {
-                id: 17,
-                name: "task-tracker/17admin_chargecodes5.png"
-            },
-            {
-                id: 18,
-                name: "task-tracker/18admin_departments.png"
-            },
-            {
-                id: 19,
-                name: "task-tracker/19admin_departments2.png"
-            },
-            {
-                id: 20,
-                name: "task-tracker/20admin_departments3.png"
-            },
-            {
-                id: 21,
-                name: "task-tracker/21admin_departments4.png"
-            },
-            {
-                id: 22,
-                name: "task-tracker/22admin_departments5.png"
-            },
-            {
-                id: 23,
-                name: "task-tracker/23admin_timelog.png"
-            },
-            {
-                id: 24,
-                name: "task-tracker/24admin_timelog2.png"
-            },
-            {
-                id: 25,
-                name: "task-tracker/25admin_timelog3.png"
-            },
-            {
-                id: 26,
-                name: "task-tracker/26admin_timelog4.png"
-            },
-            {
-                id: 27,
-                name: "task-tracker/27dashboard.png"
-            },
-            {
-                id: 28,
-                name: "task-tracker/28dashboard2.png"
-            },
-            {
-                id: 29,
-                name: "task-tracker/29dashboard3.png"
-            },
-            {
-                id: 30,
-                name: "task-tracker/responsive/1admin_timelog.png"
-            },
-            {
-                id: 31,
-                name: "task-tracker/responsive/2admin_timelog2.png"
-            },
-            {
-                id: 32,
-                name: "task-tracker/responsive/3dashboard.png"
-            },
-            {
-                id: 33,
-                name: "task-tracker/responsive/4dashboard2.png"
+                name: "ngo/10mobile.png"
             }
         ]
     }
