02ae41c673eb9d1105d3370deb4e48a42aa604a4
pulze <malagodap2@gmail.com>
2020-04-06 14:20:32 +0530

    Add new details to resume ..

--- db/dbResume.js
+++ db/dbResume.js
@@ -4,32 +4,58 @@ let resume =  [
         company_name:"eBEYONDS (Pvt) Ltd",
         duration:"Jun 2016 - Mar 2017",
         post:"Associate web Developer",
-        description:"cssdfsdfsd fssfsdfdsfdsf",
-        list:['HTML5','CSS3','Responsive Design','JavaScript']
+        description:"Provides professional services in Web Solutions, Creative Designing Services, Rich Media Management Services, Animation Solutions and IT Solutions.",
+        list:[
+            'Develop web solutions for clients in USA using PHP, HTML5, CSS, JavaScript, and jQuery. ',
+            'Create website layout/user interface by using standard HTML/CSS practices.',
+            'Create and maintain software documentation (validation report, xenu report and check list).',
+            'Be responsible for maintaining, expanding, and scaling the web sites.',
+            'Bug Fixing and optimizing web sites.'
+        ]
     },
     {
         id: 2,
         company_name:"Unuan (Pvt) Ltd",
         duration:"May 2017 - Aug 2018",
         post:"UI Engineer",
-        description:"cssdfsdfsd fssfsdfdsfdsf",
-        list:['HTML5','CSS3','Responsive Design','JavaScript']
+        description:"Creates Beautiful web solution and Digital Experiences that help people making better choices.",
+        list:[
+            'Develop customer facing UI to web sites and web applications.',
+            'Bug fixing and improve code.',
+            'Develop web pages with HTML, JavaScript and CSS',
+            'Make HTML, CSS and JavaScript to work well across browser platforms.',
+            'Optimize CSS, JavaScript and UI code for best performance.',
+            'Hosting web sites and maintaining the server.',
+            'Involve with customer to understand needs and attend to feedback.'
+        ]
     },
     {
         id: 3,
         company_name:"myDigitalOffice (Pvt) Ltd",
         duration:"Aug 2018 - Apr 2019",
         post:"Software Engineer (Front-end)",
-        description:"cssdfsdfsd fssfsdfdsfdsf",
-        list:['HTML5','CSS3','Responsive Design','JavaScript']
+        description:"myDigitalOffice provides Hotel Information Management and Back-Office Automation Solutions.",
+        list:[
+            'Bringing mockups to life using HTML, CSS, and JavaScript.',
+            'Building websites and apps that are user-friendly, responsive and usable.',
+            'Monitoring website performance and rectifying front-end-related issues.',
+            'Prepare all production documents on Adobe XD, Photoshop, Sketch (Zeplin) andtranslate same into HTML templates and introduce new features into same.',
+            'Testing to identify bugs and technical issues before and after deploying.',
+            'Build reusable code and libraries for future use.',
+            'Help back-end developers with coding and troubleshooting.',
+            'Stay up-to-date on emerging technologies.',
+            'Collaborate with other team members.'
+        ]
     },
     {
         id: 4,
         company_name:"SML Technology Solutions Lanka (Pvt) Ltd",
         duration:"Apr 2019 - Present",
         post:"Software Engineer - React Js",
-        description:"cssdfsdfsd fssfsdfdsfdsf",
-        list:['HTML5','CSS3','Responsive Design','JavaScript']
+        description:"SML is a global business and branding solutions provider.",
+        list:[
+            'Currently working on React JS Project'
+        ]
     }
 ]
 
