58f690873d11ba33b1df6e08b715f96467ec8cf3
pulze <malagodap2@gmail.com>
2020-04-06 11:35:34 +0530

    new changes...

--- db/dbResume.js
+++ db/dbResume.js
@@ -1,33 +1,33 @@
 let resume =  [
     {
         id: 1,
-        company_name:"Facebook",
-        duration:"2012-2014",
-        post:"Web Developer",
+        company_name:"eBEYONDS (Pvt) Ltd",
+        duration:"Jun 2016 - Mar 2017",
+        post:"Associate web Developer",
         description:"cssdfsdfsd fssfsdfdsfdsf",
         list:['HTML5','CSS3','Responsive Design','JavaScript']
     },
     {
         id: 2,
-        company_name:"22Facebook",
-        duration:"2012-2014",
-        post:"Web Developer",
+        company_name:"Unuan (Pvt) Ltd",
+        duration:"May 2017 - Aug 2018",
+        post:"UI Engineer",
         description:"cssdfsdfsd fssfsdfdsfdsf",
         list:['HTML5','CSS3','Responsive Design','JavaScript']
     },
     {
         id: 3,
-        company_name:"22Facebook",
-        duration:"2012-2014",
-        post:"Web Developer",
+        company_name:"myDigitalOffice (Pvt) Ltd",
+        duration:"Aug 2018 - Apr 2019",
+        post:"Software Engineer (Front-end)",
         description:"cssdfsdfsd fssfsdfdsfdsf",
         list:['HTML5','CSS3','Responsive Design','JavaScript']
     },
     {
         id: 4,
-        company_name:"22Facebook",
-        duration:"2012-2014",
-        post:"Web Developer",
+        company_name:"SML Technology Solutions Lanka (Pvt) Ltd",
+        duration:"Apr 2019 - Present",
+        post:"Software Engineer - React Js",
         description:"cssdfsdfsd fssfsdfdsfdsf",
         list:['HTML5','CSS3','Responsive Design','JavaScript']
     }
